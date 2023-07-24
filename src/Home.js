1c2219f73f05b19acc0cbdc2fd3b8b55c39c3ffe
Sonakshi Arora <compsci.materials@gmail.com>
2023-07-24 18:54:44 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -9,8 +9,7 @@ const Home = () => {
            {error && <div> { error }</div>}
            {isPending && <div>Loading...</div>}
            {blogs && <BlogList  blogs = {blogs} title = "All blogs"/> }
-           {/* <BlogList  blogs = {blogs.filter((blog) => blog.author=== 'mario')} title = "Mario's blogs" handleDelete={handleDelete}/>  */}
-            {/* <button onClick={() => setName('Luigi')}>Change name</button> */}
+         
         </div>
      );
 }
